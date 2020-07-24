60ddc052ae6e0257f6b6a2d05ceef1d80cfebb9a
qiuyu-dev <75387273@qq.com>
2020-07-24 23:05:53 +0800

    add PurchaseOrderPayConfirm

---
+++ src/components/admin/content/PurchaseOrderPayConfirmEdit.vue
@@ -0,0 +1,184 @@
+<template>
+  <div style="text-align: left">
+    <!-- <el-button class="add-button" @click="dialogFormVisible = true">付费确认</el-button> -->
+    <el-dialog
+      title="收款确认"
+      :visible.sync="dialogFormVisible"
+      @close="clear">
+      
+      <el-form :model="purchaseOrderPayConfirmForm" style="text-align: left" ref="purchaseOrderPayConfirmForm">
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="服务批次" :label-width="formLabelWidth" prop="batchNumber">
+              <el-input v-model="purchaseOrderPayConfirmForm.batchNumber" autocomplete="off" placeholder="服务批次" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="服务商" :label-width="formLabelWidth" prop="payId">
+              <el-input v-model="purchaseOrderPayConfirmForm.payId" autocomplete="off" placeholder="服务商" disabled></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="8">
+            <el-form-item label="有效客户数" :label-width="formLabelWidth" prop="effectiveNumber">
+              <el-input v-model="purchaseOrderPayConfirmForm.effectiveNumber" placeholder="有效客户数" autocomplete="off" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="服务总单价" :label-width="formLabelWidth" prop="price">
+              <el-input v-model="purchaseOrderPayConfirmForm.price" autocomplete="off" disabled></el-input>
+            </el-form-item>
+          </el-col>
+           <el-col :span="8">
+            <el-form-item label="预付款" :label-width="formLabelWidth" prop="prepayment">
+              <el-input v-model="purchaseOrderPayConfirmForm.prepayment" autocomplete="off" disabled></el-input>
+            </el-form-item>
+           </el-col>
+        </el-row>
+        <el-row :gutter="10">
+           <el-col :span="8">
+              <el-form-item label="开始日期" :label-width="formLabelWidth">
+              <el-form-item prop="beginTime">
+                  <el-date-picker type="date" placeholder="选择日期" v-model="purchaseOrderPayConfirmForm.beginTime" value-format="yyyy-MM-dd" style="width: 135px" disabled></el-date-picker>
+              </el-form-item>
+              </el-form-item>
+           </el-col>
+           <el-col :span="8">
+              <el-form-item label="结束日期" :label-width="formLabelWidth">
+              <el-form-item prop="endTime">
+                  <el-date-picker type="date" placeholder="选择日期" v-model="purchaseOrderPayConfirmForm.endTime" value-format="yyyy-MM-dd" style="width: 135px" disabled></el-date-picker>
+              </el-form-item>
+              </el-form-item>
+           </el-col>
+           <el-col :span="8">
+             <el-form-item label="应收款" :label-width="formLabelWidth" prop="receivable" required>
+              <el-input v-model="purchaseOrderPayConfirmForm.receivable" autocomplete="off" ></el-input>
+             </el-form-item> 
+           </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="付费日期" :label-width="formLabelWidth">
+            <el-form-item prop="payTime">
+                <el-date-picker type="date" placeholder="选择日期" v-model="purchaseOrderPayConfirmForm.payTime" value-format="yyyy-MM-dd" style="width: 135px" disabled></el-date-picker>
+            </el-form-item>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="付费凭证" :label-width="formLabelWidth2" prop="payImg">
+              <el-input v-model="purchaseOrderPayConfirmForm.payImg" autocomplete="off" placeholder="图片 URL" style="width: 230px" readonly></el-input>
+              <!-- <img-upload @onUpload="uploadImg" ref="imgUpload"></img-upload> -->
+            </el-form-item>
+          </el-col>          
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="24">
+            <el-form-item label="备注" :label-width="formLabelWidth" prop="remark">
+            <el-input v-model="purchaseOrderPayConfirmForm.remark" autocomplete="off" readonly></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="24">
+            <el-form-item label="确认备注" :label-width="formLabelWidth" prop="confirmRemark">
+            <el-input v-model="purchaseOrderPayConfirmForm.confirmRemark" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>  
+        </el-row>
+        <el-form-item prop="id" style="height: 0">
+          <el-input type="hidden" v-model="purchaseOrderPayConfirmForm.id" autocomplete="off"></el-input>
+        </el-form-item>                                  
+        </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>        
+        <el-button type="primary" @click="onSubmit()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import ImgUpload from './ImgUpload'
+  export default {
+    name: 'purchaseOrderPayConfirmEdit',
+    props: ['msg'],
+    components: {ImgUpload},
+    objData: {},
+    num: '',
+    data () {
+      return {
+        dialogFormVisible: false,
+        formLabelWidth: '90px',
+        formLabelWidth2: '80px',
+        purchaseOrderPayConfirmForm: {
+          id: '',
+          batchNumber: '',
+          cname: '',
+          effectiveNumber: '',
+          price: '',
+          prepayment: '',
+          receivable: '',
+          beginTime: '',
+          endTime: '',
+          payTime: '',
+          remark: '',
+          payImg: '',
+          confirmRemark: ''          
+        },        
+      }
+    },
+    methods: {
+      clear () {
+        this.purchaseOrderPayConfirmForm = {
+          id: '',
+          batchNumber: '',
+          name: '',
+          effectiveNumber: '',
+          price: '',
+          prepayment: '',
+          receivable: '',
+          beginTime: '',
+          endTime: '',
+          payTime: '',
+          remark: '',
+          payImg: '',
+          confirmRemark: ''          
+        }
+        this.$refs.purchaseOrderPayConfirmForm.resetFields()        
+      },
+      onSubmit () {
+        if(this.purchaseOrderPayConfirmForm.receivable == null||this.purchaseOrderPayConfirmForm.receivable == 0){
+          this.$alert("请输入应收款", '提示', {
+                  confirmButtonText: '确定'
+                })
+          return 
+        }
+        //alert(this.purchaseOrderPayConfirmForm.id +"/" + this.purchaseOrderPayConfirmForm.receivable + "/" +this.purchaseOrderPayConfirmForm.confirmRemark)
+        this.$axios.post('/admin/content/purchaseorder/payconfirm', {
+                id: this.purchaseOrderPayConfirmForm.id,
+                receivable: this.purchaseOrderPayConfirmForm.receivable,
+                confirmRemark: this.purchaseOrderPayConfirmForm.confirmRemark           
+              }).then(resp => {
+                if (resp && resp.data.code === 200) {
+                  this.dialogFormVisible = false
+                  this.$emit('onSubmit')
+                }
+            })       
+      },
+      uploadImg () {
+        this.purchaseOrderPayConfirmForm.payImg = this.$refs.imgUpload.url
+      }      
+    }
+  }
+</script>
+
+<style scoped>
+  .add-button {
+    margin: 0px 10px 0 10px;
+  }
+  .select {
+    width: 120px;
+    margin: 0px 10px 0 0px;
+  }
+</style>
---
+++ src/components/admin/content/PurchaseOrderPayConfirmManagement.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <el-row style="margin: 18px 0px 0px 18px ">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/admin/dashboard' }">管理中心</el-breadcrumb-item>
+        <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        <el-breadcrumb-item>付费确认</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-row>
+    <el-card style="margin: 18px 2%;width: 95%">
+      <el-table
+        ref="multipleTable"
+        :data="datas"
+        stripe
+        style="width: 100%"
+        :max-height="tableHeight"
+        @selection-change="handleSelectionChange">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <!-- <el-table-column
+          prop="policyNumber"
+          label="保单号"
+          width="100">
+        </el-table-column>
+        <el-table-column
+          prop="product"
+          label="产品"
+          fit>
+        </el-table-column> -->
+        <el-table-column
+          prop="batchNumber"
+          label="服务批号"
+          show-overflow-tooltip
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="payId"
+          label="服务商"          
+          fit>
+        </el-table-column>
+        <el-table-column
+          prop="effectiveNumber"
+          label="有效客户数"
+          width="120"          
+          fit>
+        </el-table-column>
+        <el-table-column
+          prop="price"
+          label="服务单价"          
+          fit>
+        </el-table-column>        
+        <el-table-column
+          prop="prepayment"
+          label="预付款"          
+          fit>
+        </el-table-column>
+        <el-table-column
+          prop="beginTime"
+          :formatter="dateFormat"
+          label="开始时间"
+          width="100"
+          fit>
+        </el-table-column>
+        <el-table-column
+          prop="endTime"
+          :formatter="dateFormat"
+          label="结束时间"
+          width="100"
+          fit>
+        </el-table-column>
+        <el-table-column
+          prop="payTime"
+          :formatter="dateFormat"
+          label="付费时间"
+          width="100"
+          fit>
+        </el-table-column>        
+        <el-table-column
+          prop="status"
+          :formatter="sFormat"
+          label="状态"
+          width="100"
+          fit>
+        </el-table-column>        
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="120">
+          <template slot-scope="scope">
+            <el-button
+              @click.native.prevent="editOpt(scope.row)"
+              type="text"
+              size="small">
+              确认付费
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-row>
+      <div style="margin: 20px 0 20px 0;float: right">
+        <purchase-order-pay-confirm-edit  @onSubmit="loadData()"  ref="purchaseOrderPayConfirmEdit"></purchase-order-pay-confirm-edit>
+        <!-- <el-button class="add-button" @click="batchFeeOpt()">付费</el-button> -->
+      </div>
+      </el-row>
+      </el-card>
+  </div>
+</template>
+
+<script>
+  import PurchaseOrderPayConfirmEdit from './PurchaseOrderPayConfirmEdit'
+  export default {
+    name: 'PurchaseOrderPayConfirmManagement',
+    components: {PurchaseOrderPayConfirmEdit},
+    data () {
+      return {
+        datas: [],
+        multipleSelection: []        
+      }
+    },
+    mounted () {
+      this.loadData()
+    },
+    computed: {
+      tableHeight () {
+        return window.innerHeight - 320
+      }
+    },
+    methods: {
+      editOpt (item) {
+        this.$refs.purchaseOrderPayConfirmEdit.dialogFormVisible = true
+        this.$refs.purchaseOrderPayConfirmEdit.purchaseOrderPayConfirmForm = {
+          id: item.id,
+          batchNumber: item.batchNumber,
+          payId: item.payId,
+          effectiveNumber: item.effectiveNumber,
+          price: item.price,
+          prepayment: item.prepayment,
+          receivable: item.receivable.toString(),
+          beginTime: item.beginTime,
+          endTime: item.endTime,
+          payTime: item.payTime,
+          remark: item.remark,
+          payImg: item.payImg,
+          confirmRemark: item.confirmRemark       
+        }       
+      },
+      loadData () {
+        var _this = this
+        this.$axios.get('/batchfee/list').then(resp => {
+          if (resp && resp.data.code === 200) {
+            _this.datas = resp.data.result
+          }
+        })
+      },
+      toggleSelection (rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row)
+          })
+        } else {
+          this.$refs.multipleTable.clearSelection()
+        }
+      },
+      handleSelectionChange (val) {
+        this.multipleSelection = val
+      },     
+      dateFormat (row, column) {
+        var date = row[column.property]
+        if (date !== null && date !== undefined) {
+          return this.$moment(date).format('YYYY-MM-DD')
+        }
+      },
+      ctFormat (row, column) {
+        var ctype = row[column.property]
+        if (ctype == '1') {
+          return '身份证'
+        } else if (ctype == '2') {
+          return '护照'
+        }
+      },
+      sFormat (row, column) {
+        var ctype = row[column.property]
+        if (ctype == '0') {
+          return '未确认'
+        } else if (ctype == '1') {
+          return '已确认'
+        } 
+      }      
+    }
+  }
+</script>
+
+<style scoped>
+</style>
